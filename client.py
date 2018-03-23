623de0c80e3d7ad08fe6049f89bb2c833daa85bc
Demo User <debian@beaglebone.localdomain>
2018-03-23 01:13:55 +0000

    fix

--- client.py
+++ client.py
@@ -22,7 +22,9 @@ class Message(Protocol):
         self.msg['count'] = 0
         self.msg['type'] = None
         self.msg['header_offset'] = 0
-        self.msg['ptr'] = -1
+        self.msg['read_busy'] = False
+        self.msg['bus_idle'] = True
+        self.msg['complete'] = False
 
     def connectionMade(self):
         self.start_stream()
@@ -39,15 +41,85 @@ class Message(Protocol):
             rate = self.config['frame_rate']
             reactor.callLater(1.0/rate, self.get_frame)
 
+    def async_msg_parser(self):
+        # process image frames
+        if self.msg['type'] == 'img':
+            # self.process_frame
+            msg_args = self.msg['buffer'].split(';')
+            if len(msg_args) < 3:
+                return None
+                
+            self.data['img_size'] = eval(msg_args[1])
+
+            self.msg['header_offset'] = len(msg_args[0] + ';' + msg_args[1] + ';')
+
+            goal_count = self['img_size'] + self.msg['header_offset']
+
+            if len(self.msg['buffer']) >= goal_count:
+                self.data['img'] = msg_args[2]
+                return True
+
+        else:
+            print("unrec datatype")
+            return False
+
+xx
+
+    def async_read_packet(self, data):
+        new_packet = self.msg['bus_idle'] == True
+        msg_type_known = self.
+
+        if new_packet:
+            self.msg['buffer'] = data
+            self.msg['count'] = len(data)
+            self.msg['bus_idle'] = False # self.set_bus_active()
+            self.msg['read_busy'] = True # self.set_bus_active()
+
+            # self.init_msg
+            self.msg['type'] = None
+
+        else:
+            self.msg['buffer'] += data
+            self.msg['count'] += len(data)
+        
+        if self.msg['type'] is None:
+            msg_args = self.msg['buffer'].split(';')
+            if len(msg_args) > 1:
+                self.msg['type'] = msg_args[0]
+        else:
+            return None
+
+        # at this point, there is enough information to run the message parser  
+        self.async_msg_parser()
+
+
+     
+        if msg_done:
+            return True # sucess!
+            
+        else:
+            return False # not down yet
+            = self.async_msg_parser()
+            
+
+        
+
     def dataReceived(self, data):
         if self.state['expecting_frame']:
+            self.async_read_packet(data)
+
+        else:
+            print("received inappropiate packet")
+
+
             if self.state['frame_start'] == False:
-                self.msg['buffer'] = ''
+
+
 
                 self.msg['buffer'] = data
                 self.msg['count']  = len(data)
 
-                # do some processing
+                # determine type of message
                 msg = self.msg['buffer']
                 msg = msg.split(';')
                 self.msg['type'] = msg[0]
